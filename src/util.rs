d3a00227e72f796db0aa3dd7cf5321c410000fad
Cem Özden <cemozden.tr@gmail.com>
2021-04-07 21:16:58 +0200

    Fixing relative path issues for extract and list commands

--- src/util.rs
+++ src/util.rs
@@ -1,28 +1,4 @@
-use std::{ffi::OsString, io::{Error, Write}, path::Path};
-
-pub fn get_path<P>(path: P) -> Option<OsString> where P: AsRef<Path> {
-    let file_path = path.as_ref();
-    if !file_path.exists() {
-        let current_dir = match std::env::current_dir() {
-            Ok(path) => path,
-            Err(err) => {
-                eprintln!("An error occured! Error: {}", err);
-                return None;
-            }
-        };
-        let file_path = Path::new(current_dir.as_path()).with_file_name(file_path);
-        if file_path.exists() {
-            return Some(OsString::from(file_path.as_os_str()));
-        }
-        else {
-            eprintln!(r"Unable to find the given path {:?}", file_path);
-            return None;
-        }
-    }
-    else {
-        return Some(OsString::from(file_path.as_os_str()));
-    }
-}
+use std::io::{Error, Write};
 
 pub fn read_pass() -> Result<String, Error> {
     print!("Enter password: ");
